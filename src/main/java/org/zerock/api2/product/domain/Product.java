9c4efa22b55eb3d69a31087f2dff1fceabedd5f1
yun9699 <cha23215@daum.net>
2024-10-11 09:35:38 +0900

    0930

--- src/main/java/org/zerock/api2/product/domain/Product.java
+++ src/main/java/org/zerock/api2/product/domain/Product.java
@@ -2,6 +2,10 @@ package org.zerock.api2.product.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Builder
 @AllArgsConstructor
@@ -20,5 +24,19 @@ public class Product {
     private int price;
 
     private ProductStatus status;
+
+    @ElementCollection
+    @CollectionTable(name = "tbl_product_tag")
+    private Set<String> tags = new HashSet<>();
+
+    public void addTag(String tag) {
+        tags.add(tag);
+    }
+    public void removeTag(String tag) {
+        tags.remove(tag);
+    }
+    public void clearTages() {
+        tags.clear();
+    }
 }
 
